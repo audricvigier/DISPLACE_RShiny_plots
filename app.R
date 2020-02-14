8a779cda6bb16a71e4e4d0614fc90119bdb9266d
Francois Bastardie <fba@aqua.dtu.dk>
2020-02-14 16:48:04 +0100

    Try a scenario-oriented plots

--- app.R
+++ app.R
@@ -2,8 +2,19 @@
 
 library(shiny)
 
-load("data/lst_loglike_weight_agg_scebaseline.RData")
+sces <- c("scebaseline", "scesizespectra")
+
+# loglike outcomes
+for (sce in sces)
+  load(paste0("data/lst_loglike_weight_agg_",sce,".RData"))
+
+# poplike outcomes
+for (sce in sces)
+  load(paste0("data/lst_popdyn_",sce,".RData"))
+
+
 source("R/polygonPlotsFromAggLoglikeFiles.R")
+source("R/polygonPlotsFromPopDynFiles.R")
 ##source("R/DISPLACE_plotting.R")
 ##the_baseline <- ""
 ##combined_name <- paste(the_baseline,"_vs_", sce, sep='')
@@ -19,6 +30,10 @@ selpop <- function() {
     nms <- names(lst_loglike_agg_weight_all_scebaseline[[1]])
     pops <- nms[startsWith(nms, "pop.")]
     pops
+}
+
+selsce <- function() {
+    sces
 }
 
 selvar <- function() {
@@ -37,15 +52,16 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            selectInput("sel.pop", "Select populations", choices = selpop(), selected = "pop.1", multiple = TRUE, selectize = FALSE),
-            selectInput("sel.var", "Select variable", choices = selvar(), selected = selvar()[1], multiple = FALSE)
-
+            selectInput("sel.sce", "Select scenarios", choices = selsce(), selected = selsce(), multiple = TRUE, selectize = FALSE),
+            selectInput("sel.var", "Select a variable", choices = selvar(), selected = "gradva", multiple = FALSE),
+            selectInput("sel.pop", "Select populations", choices = selpop(), selected = "pop.1", multiple = TRUE, selectize = FALSE)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
             plotOutput("linePlot"),
-            plotOutput("linePlot2")
+            plotOutput("linePlot2"),
+            plotOutput("linePlot3")
         )
     )
 )
@@ -58,20 +74,19 @@ server <- function(input, output) {
             a_variable = input$sel.var,
             nby = 5,
             a_set_of_scenarios= c(
-                "scebaseline",
-                "scebaseline",
-                "scebaseline"),
+                input$sel.sce),
             the_scenario_names= c(
-                "Baseline",
-                "Same",
-                "Same again") ,
+                input$sel.sce) ,
             name_set_of_sces= "setA",
             selected="_selected_set1_",
             export=FALSE,
-            a_xlab="# months",
-            add_legend=TRUE,
-            color_legend= c(rgb(94/255,79/255,162/255,0.5), rgb (158/255,1/255,66/255,0.5),
-                            rgb(140/255,81/255,10/255,0.4), rgb(1,0,0,0.5), rgb(0,0.5,1,0.5), rgb(0,1,0.5,0.5), rgb(1,0,0.5,0.5), rgb(0,0,0,0.2)) ,
+            a_ylab = switch(input$sel.var,
+                            gradva = "Acc. GVA (mio Euro)",
+                            rev_from_av_prices = "Income from landings (mio Euro)",
+                            rev_explicit_from_av_prices = "Income from landings (mio Euro)",
+                           "Accumulated Gross Added Value (millions Euro)"),
+                  add_legend=TRUE,
+                  color_legend= c(rgb(94/255,79/255,162/255,0.5), rgb (158/255,1/255,66/255,0.5), rgb(140/255,81/255,10/255,0.4), rgb(1,0,0,0.5), rgb(0,0.5,1,0.5), rgb(0,1,0.5,0.5), rgb(1,0,0.5,0.5), rgb(0,0,0,0.2)),
             a_width=3500,
             a_height=1000
 
@@ -83,9 +98,17 @@ server <- function(input, output) {
         ym <- ym2date(lst_loglike_agg_weight_all_scebaseline[[1]]$year.month)
         matplot(ym, lst_loglike_agg_weight_all_scebaseline[[1]][, names(lst_loglike_agg_weight_all_scebaseline[[1]]) %in% input$sel.pop],
                 type = "l", ylab = "Kg", xlab = "Year")
-        title(main = "Population development over time")
+        title(main = "Catch development over time")
     })
-
+    
+    
+     output$linePlot3 <- renderPlot({
+        plot_popdyn (sces=input$sel.sce,
+                      explicit_pops= input$sel.pop,
+                      sum_all=FALSE
+                      ) 
+     })
+     
     # output$boxplot <- renderPlot({
     #
     # })
