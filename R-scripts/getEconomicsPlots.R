85bd80a7de17f79986c40147a0616ace96069b60
audricvigier <a.vigier@qub.ac.uk>
2021-06-10 17:48:45 +0100

    Finished time series plot function for economics variables

--- R-scripts/getEconomicsPlots.R
+++ R-scripts/getEconomicsPlots.R
@@ -91,7 +91,9 @@ economicFeatures = economicFeatures%>%
 
 EconomicsPertrip = EconomicsPertrip %>% 
   merge(economicFeatures, by=c("VE_REF")) %>% 
-  mutate(nonFuelvariableCost=Other_variable_costs_per_unit_effort*cumsteaming)
+  mutate(nonFuelvariableCost=Other_variable_costs_per_unit_effort*cumsteaming) %>% 
+  mutate(freq_metiers = sapply(as.character(freq_metiers), function(x) strsplit(x, split="\\(")[[1]][2]))%>% 
+  mutate(freq_metiers = as.numeric(sapply(as.character(freq_metiers), function(x) strsplit(x, split=")")[[1]][1])))
 
 
 #Explore the variables to understand them (which are cumulative through the whole time series?)
@@ -112,12 +114,12 @@ EconomicsPertrip = EconomicsPertrip %>%
 # WARNING : NetPresentValue LOOKS cumulative
 # WARNING : numTrips is cumulative
                                  
-#Sanity checks
-
-# rev_from_av_prices == rev_explicit_from_av_prices except for pelagic metiers OK
-unique(EconomicsPertrip$freq_metiers[which(EconomicsPertrip$rev_from_av_prices != EconomicsPertrip$rev_explicit_from_av_prices)])
-# Metiers 5, 16 and 11 : all PT. What is the ratio between both revenues? ALL GOOD
-range(1-(EconomicsPertrip$rev_from_av_prices[which(EconomicsPertrip$rev_from_av_prices != EconomicsPertrip$rev_explicit_from_av_prices)]-EconomicsPertrip$rev_explicit_from_av_prices[which(EconomicsPertrip$rev_from_av_prices != EconomicsPertrip$rev_explicit_from_av_prices)])/EconomicsPertrip$rev_from_av_prices[which(EconomicsPertrip$rev_from_av_prices != EconomicsPertrip$rev_explicit_from_av_prices)])
+##Sanity checks
+#
+## rev_from_av_prices == rev_explicit_from_av_prices except for pelagic metiers OK
+#unique(EconomicsPertrip$freq_metiers[which(EconomicsPertrip$rev_from_av_prices != EconomicsPertrip$rev_explicit_from_av_prices)])
+## Metiers 5, 16 and 11 : all PT. What is the ratio between both revenues? ALL GOOD
+#range(1-(EconomicsPertrip$rev_from_av_prices[which(EconomicsPertrip$rev_from_av_prices != EconomicsPertrip$rev_explicit_from_av_prices)]-EconomicsPertrip$rev_explicit_from_av_prices[which(EconomicsPertrip$rev_from_av_prices != EconomicsPertrip$rev_explicit_from_av_prices)])/EconomicsPertrip$rev_from_av_prices[which(EconomicsPertrip$rev_from_av_prices != EconomicsPertrip$rev_explicit_from_av_prices)])
 
 # revenue = income
 # revenue - fuelcosts = gradva . Sometimes, a difference of 1 EUR. Why? Rounding error?
@@ -173,21 +175,22 @@ range(1-(EconomicsPertrip$rev_from_av_prices[which(EconomicsPertrip$rev_from_av_
 #     geom_line(alpha=0.1)+
 #     theme_minimal()
   
-# Net profit Margin = Net Profit / revenue. NOT GOOD
-check = EconomicsPertrip %>%
-    arrange(VE_REF,TStepDep,TStep) %>%
-    group_by(VE_REF) %>%
-    mutate(cumRevenue = cumsum(rev_from_av_prices)) %>%
-    ungroup() %>% 
-    mutate(NetProfitMarginEstim = 100*NetProfit/cumRevenue) %>%
-    #mutate(NetProfitMarginEstim = replace(NetProfitMarginEstim,cumRevenue<=0,0)) %>%
-    mutate(SanityCheck = abs(NetProfitMarginEstim - NetProfitMargin))%>%
-    #filter(SanityCheck<100000) %>% 
-    ggplot(aes(x=TStep,y=SanityCheck,group=VE_REF))+
-      geom_line(alpha=0.1)+
-      theme_minimal()
+# Net profit Margin = Net Profit / revenue. Discrepances up to 1500 EUR for very small vessels, due to discrepancies of a few EUR in cum revenue. Probably due to rounding errors when DISPLACE exports stuff.
+# check = EconomicsPertrip %>%
+#     arrange(VE_REF,TStepDep,TStep) %>%
+#     group_by(VE_REF) %>%
+#     mutate(cumRevenue = cumsum(rev_from_av_prices)) %>%
+#     ungroup() %>% 
+#     mutate(NetProfitMarginEstim = 100*NetProfit/cumRevenue) %>%
+#     mutate(NetProfitMarginEstim = replace(NetProfitMarginEstim,cumRevenue<=0,NA)) %>% # Should be 0, but buggy, so NA.
+#     mutate(SanityCheck = NetProfitMarginEstim/NetProfitMargin)%>%
+#     #filter(SanityCheck=1500) %>% 
+#     ggplot(aes(x=TStep,y=SanityCheck,group=VE_REF))+
+#       geom_line(alpha=0.1)+
+#       theme_minimal()+
+#       expand_limits(y=0)
 
-# save(FestimatesYear,file=paste(general$main.path,general$case_study,sce,"output/forFPlots.Rdata",sep="/"))
+save(EconomicsPertrip,file=paste(general$main.path,general$case_study,sce,"output/forEconomicsPlots.Rdata",sep="/"))
 
 ##################
 ###
@@ -195,35 +198,95 @@ check = EconomicsPertrip %>%
 ###
 ##################
 
-getFTimeSeries = function(FestimatesYear, fbar=TRUE){
+returnLastNonNAValue <- function(values,pos){
+  lastValues=values[1:pos]
+  mostRecent=max(which(!is.na(lastValues)))
+  return(lastValues[mostRecent])
+}
+
+getEconomicTimeSeries = function(data4plot = EconomicsPertrip,variable="revenue",cumulTime=T,metChoice=0, facet=F){
+
+  legendY = paste(variable,"\n","(EUR)",sep="")
+  legendTitle =  paste(variable,"\n",sce,sep="")
+  #if(averageTrip)legendY = paste("Average\n",variable,"\nper trip\n(EUR)",sep="")
+  if(cumulTime) legendTitle = paste("Cumul of ",variable,"\n",sce,sep="")
+  #if(cumulTime & averageTrip) legendY = paste("Cumulative\naverage\n",variable,"\nper trip\n(EUR)",sep="")
   
-  if(!fbar){
-    plot2return = FestimatesYear %>% 
-      mutate(age=factor(age)) %>% 
-      ggplot(aes(x=year,y=value,colour=age,linetype=age,shape=age))+
-        geom_line(size=1)+
-        geom_point(size=2)+
-        facet_wrap(~spp,scales="free_y")+
-        labs(x="Time step (year)",y="Fishing\nmortality",colour="Age",linetype="Age",shape="Age", title = paste("Fishing mortality per age class\n",sce,sep=""))+
-        scale_colour_manual(name="Age", values=rep(scales::hue_pal()(5),3)[1:11])+
-        scale_linetype_manual(name="Age", values=c(rep(1:2,each=5),3))+
-        scale_shape_manual(name="Age", values=c(rep(c(15:16),each=5),17))+
-        expand_limits(y=0)+
-        theme_minimal()+
-        theme(axis.title.y = element_text(angle=0,vjust=0.5))
+  if(variable=="revenue") data4plot$value = data4plot$rev_from_av_prices
+  if(variable=="profit") data4plot$value = data4plot$gradva
+  if(variable=="fuelcost") data4plot$value = data4plot$fuelcost
+  if(variable=="nonFuelvariableCost") data4plot$value = data4plot$nonFuelvariableCost
+  if(variable=="GVA") data4plot$value = data4plot$GVA
+  if(variable=="GrossProfit") data4plot$value = data4plot$GrossProfit
+  if(variable=="LabourSurplus") data4plot$value = data4plot$LabourSurplus
+  if(variable=="NetProfit") data4plot$value = data4plot$NetProfit
+  if(variable=="NetProfitMargin") data4plot$value = data4plot$NetProfitMargin
+  
+  if (!is.na(metChoice)) data4plot = subset(data4plot,freq_metiers%in%metChoice)
+  if (is.na(metChoice)) data4plot$freq_metiers="All"
+
+  if(variable %in%c("revenue","profit","fuelcost","nonFuelvariableCost")){
+    data4plot = data4plot %>% 
+      select(value,TStep,freq_metiers) %>% 
+      group_by(TStep,freq_metiers) %>% 
+      summarize(value=sum(value))%>% 
+      ungroup()
+    
+    if (!is.na(metChoice)) data4plot= mutate(data4plot, freq_metiers = sapply(freq_metiers, function(x) metierNames$name[metierNames$metierId==x] ))
+    
+    if(cumulTime){
+      data4plot = data4plot %>% 
+        arrange(TStep,freq_metiers) %>% 
+        group_by(freq_metiers) %>% 
+        mutate(value=cumsum(value)) %>% 
+        ungroup()
+    }
   }
   
-  if(fbar){
-    plot2return = FestimatesYear %>% 
-      ggplot(aes(x=year,y=Fbar))+
-        geom_line(size=1)+
-        geom_point(size=2)+
-        facet_wrap(~spp,scales="free_y")+
-        labs(x="Time step (year)",y="Fishing\nmortality", title = paste("Fbar\n",sce,sep=""))+
-        expand_limits(y=0)+
-        theme_minimal()+
-        theme(axis.title.y = element_text(angle=0,vjust=0.5))
+  if(!variable %in%c("revenue","profit","fuelcost","nonFuelvariableCost")){
+    data4plot = data4plot %>% 
+      select(value,TStep,freq_metiers,VE_REF) %>% 
+      complete(nesting(freq_metiers, VE_REF),TStep=full_seq(TStep,1)) %>% 
+      group_by(VE_REF) %>% #If for the first time step, GVA is NA, then replacce by 0; do not change it otherwise
+      mutate(value=replace(value,TStep==min(TStep) & is.na(value),0)) %>% 
+      ungroup() %>% 
+      arrange(freq_metiers,VE_REF,TStep) %>% 
+      fill(value,.direction="down") %>% 
+      group_by(freq_metiers,TStep) %>% 
+      summarize(value=sum(value))%>% 
+      ungroup()
+    
+      if (!is.na(metChoice)) data4plot= mutate(data4plot, freq_metiers = sapply(freq_metiers, function(x) metierNames$name[metierNames$metierId==x] ))
+    
+    if(!cumulTime){
+      data4plot = data4plot %>% 
+        arrange(freq_metiers,TStep) %>% 
+        group_by(freq_metiers) %>% 
+        mutate(value=c(min(value[TStep==min(TStep)]),diff(value))) %>% 
+        ungroup()
+    }
   }
+
+  plot2return = ggplot(data4plot,aes(x=TStep,y=value))+#,colour=freq_metiers,linetype=freq_metiers,shape=freq_metiers))+
+    geom_vline(xintercept=months$TStep[months$TStep<max(data4plot$TStep)],linetype="dotted")+
+    labs(x="Time step (hour)",y=legendY,colour="Métier",linetype="Métier",shape="Métier", title = legendTitle)+
+    expand_limits(y=0)+
+    theme_minimal()+
+    theme(axis.title.y = element_text(angle=0,vjust=0.5))
   
+  if(facet){
+    plot2return = plot2return + facet_wrap(~freq_metiers,scales="free_y")+
+                                        geom_line(size=1)+
+                                        geom_point(size=2)
+  }
+  if(!facet){
+    plot2return = plot2return +  scale_colour_manual(name="Métier", values=rep(scales::hue_pal()(5),4)[1:length(metChoice)])+
+                                  scale_linetype_manual(name="Métier", values=c(rep(1:4,each=5),3)[1:length(metChoice)])+
+                                  scale_shape_manual(name="Métier", values=c(rep(c(15:18),each=5),17)[1:length(metChoice)])+
+                                  geom_line(size=1,aes(colour=freq_metiers,linetype=freq_metiers))+
+                                  geom_point(size=2,aes(colour=freq_metiers,shape=freq_metiers))
+  }
   return(plot2return)
 }
+
+#getEconomicTimeSeries(EconomicsPertrip,variable,cumulTime,metChoice,facet)
